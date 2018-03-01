70514fc471b2622a2d17b620e44c3e025b3bf3f2
farhatsarfraz23@outlook.com <farahsarfraz@gmail.com>
2018-03-01 21:55:18 +0500

    validation on email

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,5 +1,8 @@
 class Chef < ApplicationRecord
 before_save { self.email = email.downcase }
 validates :chefname, presence: true, length: { maximum: 16 } 
-validates :email, presence: true, length: { maximum: 35 }
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+validates :email, presence: true, length: { maximum: 35 },
+       format: { with: VALID_EMAIL_REGEX }, 
+       uniqueness: { case_sensitive: false}
 end
