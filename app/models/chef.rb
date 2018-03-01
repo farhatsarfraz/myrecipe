f7046dd6449b0bad4e32517ea933556939fefbd9
farhatsarfraz23@outlook.com <farahsarfraz@gmail.com>
2018-03-01 23:05:01 +0500

    add foreign key and relation in tables

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -5,4 +5,5 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 validates :email, presence: true, length: { maximum: 35 },
        format: { with: VALID_EMAIL_REGEX }, 
        uniqueness: { case_sensitive: false}
+       has_many :recipes
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
 class Recipe < ApplicationRecord
 validates :name, presence: true, length: {minmum: 5,maximum: 500}
+belongs_to :chef
+validates :chef_id. presence: true
 end
